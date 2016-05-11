eafb3fd5be43c22f11b80999c50350b9b60845ab
RealData <grigory.andreev@gmail.com>
2016-05-11 08:26:22 -0700

    jsmeOnLoad() function can now load several jsme editors

--- inst/htmlwidgets/jsme.js
+++ inst/htmlwidgets/jsme.js
@@ -1,64 +1,66 @@
-readMolecule = function(el, structure) {
+(function() {
+  var readMolecule = function(el, structure) {
     $('#'+el).data('jsme').readMolecule(structure);
-}
+  };
 
-readMolFile = function(el, structure) {
+  var readMolFile = function(el, structure) {
     $('#'+el).data('jsme').readMolFile(structure);
-}
+  };
 
-reset = function(el) {
+  var reset = function(el) {
     $('#'+el).data('jsme').reset();
-}
+  };
 
-smiles = function(el, inputEl) {
+  var smiles = function(el, inputEl) {
     Shiny.onInputChange(inputEl, $('#'+el).data('jsme').smiles());
-}
+  };
 
-molFile = function(el, inputEl) {
+  var molFile = function(el, inputEl) {
     Shiny.onInputChange(inputEl, $('#'+el).data('jsme').molFile());
+  };
 
-}
-
-jmeFile = function(el, inputEl) {
+  var jmeFile = function(el, inputEl) {
     Shiny.onInputChange(inputEl, $('#'+el).data('jsme').jmeFile());
+  };
 
-}
+  Shiny.addCustomMessageHandler('useJME',
+    function(message) {
+      readMolecule(message.el, message.jmeFile);
+    }
+  );
 
-Shiny.addCustomMessageHandler('useJME',
-  function(message) {
-   readMolecule(message.el, message.jmeFile);
-  }
-);
+  Shiny.addCustomMessageHandler('useMOL',
+    function(message) {
+      readMolFile(message.el, message.molFile);
+    }
+  );
 
-Shiny.addCustomMessageHandler('useMOL',
-  function(message) {
-    readMolFile(message.el, message.molFile);
-  }
-);
+  Shiny.addCustomMessageHandler('resetEditor',
+    function(message) {
+      reset(message.el);
+    }
+  );
 
-Shiny.addCustomMessageHandler('resetEditor',
-  function(message) {
-   reset(message.el);
-  }
-);
+  Shiny.addCustomMessageHandler('smiles',
+    function(message) {
+      smiles(message.el, message.inputEl);
+    }
+  );
 
-Shiny.addCustomMessageHandler('smiles',
-  function(message) {
-   smiles(message.el, message.inputEl);
-  }
-);
+  Shiny.addCustomMessageHandler('molFile',
+    function(message) {
+      molFile(message.el, message.inputEl);
+    }
+  );
 
-Shiny.addCustomMessageHandler('molFile',
-  function(message) {
-   molFile(message.el, message.inputEl);
-  }
-);
+  Shiny.addCustomMessageHandler('jmeFile',
+    function(message) {
+      jmeFile(message.el, message.inputEl);
+    }
+  );
+})();
 
-Shiny.addCustomMessageHandler('jmeFile',
-  function(message) {
-   jmeFile(message.el, message.inputEl);
-  }
-);
+
 
 HTMLWidgets.widget({
 
@@ -68,31 +70,43 @@ HTMLWidgets.widget({
 
   factory: function(el, width, height) {
 
+
+
+    if (typeof window.jsmeArray === 'undefined') window.jsmeArray = [];
+    window.jsmeArray.push({el: el.id, width: width, height: height});
+
+    jsmeOnLoad = function() {
+      if (typeof window.jsmeArray !== 'undefined') {
+        window.jsmeArray.forEach(
+          function(v) {
+            if (typeof $('#'+v.el).data('jsme') === 'undefined') {
+              $('#'+v.el).data('jsme', new JSApplet.JSME(v.el, '100%', '100%'));
+              $('#'+v.el).data('jsme').setSize(v.width, v.height);
+            }
+          }
+        );
+      }
+	  };
+
+
     return {
 
       renderValue: function() {
 
-jsmeOnLoad = function() {
+        if ((typeof $('#'+el.id).data('jsme') === 'undefined') && (typeof JSApplet !== 'undefined')) {
+          $('#'+el.id).data('jsme', new JSApplet.JSME(el.id, '100%', '100%')); // jsmeOnLoad();
+        }
 
-					//window.jsmeObject = new JSApplet.JSME(el.id, '100%', '100%');
-					//$(el).data('jsme', jsmeObject);
-
-//var jsmeObject = new JSApplet.JSME(el.id, '100%', '100%');
-					$('#'+el.id).data('jsme', new JSApplet.JSME(el.id, '100%', '100%'));
-
-	   };
-
-    //if (window.jsmeObject === undefined) jsmeOnLoad();
-    if ($('#'+el.id).data('jsme') === undefined) jsmeOnLoad();
-//$(el).data('jsme', new JSApplet.JSME(el.id, '100%', '100%'));
-
-        },
+      },
 
       resize: function(width, height) {
 
-    $('#'+el.id).data('jsme').setSize(width, height);
+        $('#'+el.id).data('jsme').setSize(width, height);
 
       }
+
     };
+
   }
+
 });
